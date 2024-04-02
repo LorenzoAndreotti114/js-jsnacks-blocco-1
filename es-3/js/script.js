397e07019a423f80a6a980429f4cfb772a507089
LorenzoAndreotti114 <lorenzoandr123@gmail.com>
2024-04-02 23:08:14 +0200

    ex 3 final

---
+++ es-3/js/script.js
@@ -0,0 +1,27 @@
+
+let button = document.getElementById("btn");
+
+button.addEventListener("click", function() {
+
+    console.log("start");
+
+    const maxNumber = 3
+
+    let totNumber = 0
+
+    for (let x = 1; x <= maxNumber; x++) { 
+        
+        let ask = parseInt(prompt())
+        console.log(ask, typeof ask)
+        totNumber += ask 
+        
+    };
+
+    console.log(totNumber);
+
+    alert(`il totale dei numeri è: ${totNumber}`)
+    
+    document.getElementById("numberShow").innerHTML += totNumber
+
+});
+
